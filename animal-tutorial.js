d568769122245d144eaf33a9477ca72ee6ff705b
Jahaziel.vaz <jahaziel.vaz@gmail.com>
2018-02-09 17:13:25 -0700

    Successful ajax get request

--- animal-tutorial.js
+++ animal-tutorial.js
@@ -1,5 +1,22 @@
 var myRequest = new XMLHttpRequest();
-var fs = require('fs');
+
+var animalDump = document.getElementById('animal-dump');
+// var button = document.querySelector('#animal-button');
+
+myRequest.open('GET', "https://learnwebcode.github.io/json-example/animals-1.json");
+
+myRequest.onload = function(){
+  var myData = JSON.parse(myRequest.responseText);
+  console.log(myData[0]);
+};
 
 
-var animals = require('./animals.json')
+// document.getElementById("animal-button").addEventListener('click', function(){
+//     animalDump.innerHTML = myRequest.responseText;
+// });
+
+
+
+
+
+myRequest.send();
